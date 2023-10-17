3e1404ca9fce337caf994b8b6559ca6daad034b4
Tigran Bayburtsyan <t@tigran.tech>
2023-10-17 10:25:12 +0300

    adding access types

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -6,6 +6,12 @@ import (
 
 func AuthMiddleware(c *gin.Context) {
 	c.Set("AuthId", "auth-token")
+
+	// Full access without auth by default
+	c.Set("AuthAccess", "read,write")
+
+	// Keeping namespace available for everyone by default
+	c.Set("AuthNamespace", "")
 	c.Next()
 }
 
