2c7bde23809bed00d3a145bd70a985c76618d84b
Tigran Bayburtsyan <t@tigran.tech>
2023-10-17 07:53:52 +0300

    making server setup separate from router setup

--- router/router.go
+++ router/router.go
@@ -11,16 +11,16 @@ func SetupGinServer() *gin.Engine {
 	r := gin.Default()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-	return r
-}
-
-func PackageRouter(r *gin.Engine, storageBackend storage.BaseStorageBackend) {
 	r.Use(middlewares.AuthMiddleware)
 
 	r.GET("/", services.HealthCheckHandler)
 
 	r.RedirectTrailingSlash = false
 
+	return r
+}
+
+func PackageRouter(r *gin.Engine, storageBackend storage.BaseStorageBackend) {
 	initNpmRoutes(r, storageBackend)
 	initPypiRoutes(r, storageBackend)
 	initContainerRoutes(r, storageBackend)
